6d64f733f2084e8abba5b11ffd4e4de514e1ddbf
Diego Chavez <di3gochav@gmail.com>
2019-09-17 12:05:25 +0200

    chenge server order

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./car-api/db.json')
-const middlewares = jsonServer.defaults({static: './build', delay: 10000})
+const middlewares = jsonServer.defaults({static: './build', delay: 1000})
 
-server.use(middlewares)
 server.use(router)
+server.use(middlewares)
 server.listen(3000, () => {
   console.log('Application is running')
 })
